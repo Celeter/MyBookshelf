88ae1451b56680a52345d24ad230fd776cbef52e
Celeter <alansky114@gmail.com>
2020-05-14 20:53:48 +0800

    up

--- app/src/main/java/com/kunfei/bookshelf/view/activity/AboutActivity.java
+++ app/src/main/java/com/kunfei/bookshelf/view/activity/AboutActivity.java
@@ -46,14 +46,6 @@ public class AboutActivity extends MBaseActivity {
     TextView tvVersion;
     @BindView(R.id.vw_version)
     CardView vwVersion;
-    @BindView(R.id.tv_donate)
-    TextView tvDonate;
-    @BindView(R.id.vw_donate)
-    CardView vwDonate;
-    @BindView(R.id.tv_scoring)
-    TextView tvScoring;
-    @BindView(R.id.vw_scoring)
-    CardView vwScoring;
     @BindView(R.id.tv_git)
     TextView tvGit;
     @BindView(R.id.vw_git)
@@ -64,10 +56,6 @@ public class AboutActivity extends MBaseActivity {
     CardView vwDisclaimer;
     @BindView(R.id.ll_content)
     LinearLayout llContent;
-    @BindView(R.id.tv_mail)
-    TextView tvMail;
-    @BindView(R.id.vw_mail)
-    CardView vwMail;
     @BindView(R.id.tv_update)
     TextView tvUpdate;
     @BindView(R.id.vw_update)
@@ -82,18 +70,10 @@ public class AboutActivity extends MBaseActivity {
     TextView tvUpdateLog;
     @BindView(R.id.vw_update_log)
     CardView vwUpdateLog;
-    @BindView(R.id.tv_home_page)
-    TextView tvHomePage;
-    @BindView(R.id.vw_home_page)
-    CardView vwHomePage;
     @BindView(R.id.tv_faq)
     TextView tvFaq;
     @BindView(R.id.vw_faq)
     CardView vwFaq;
-    @BindView(R.id.tv_share)
-    TextView tvShare;
-    @BindView(R.id.vw_share)
-    CardView vwShare;
 
     private MoDialogHUD moDialogHUD;
     private String[] allQQ = new String[]{"(公众号)开源阅读", "(QQ群)701903217", "(QQ群)805192012", "(QQ群)773736122", "(QQ群)981838750"};
@@ -135,13 +115,9 @@ public class AboutActivity extends MBaseActivity {
 
     @Override
     protected void bindEvent() {
-        vwDonate.setOnClickListener(view -> DonateActivity.startThis(this));
-        vwScoring.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW, "market://details?id=" + getPackageName()));
-        vwMail.setOnClickListener(view -> openIntent(Intent.ACTION_SENDTO, "mailto:kunfei.ge@gmail.com"));
         vwGit.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW, getString(R.string.this_github_url)));
         vwDisclaimer.setOnClickListener(view -> moDialogHUD.showAssetMarkdown("disclaimer.md"));
         vwUpdate.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW, getString(R.string.latest_release_url)));
-        vwHomePage.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW, getString(R.string.home_page_url)));
         vwQq.setOnClickListener(view -> {
             PopupMenu popupMenu = new PopupMenu(AboutActivity.this, view);
             for (String qq : allQQ) {
@@ -155,22 +131,6 @@ public class AboutActivity extends MBaseActivity {
         });
         vwUpdateLog.setOnClickListener(view -> moDialogHUD.showAssetMarkdown("updateLog.md"));
         vwFaq.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW, "https://mp.weixin.qq.com/s?__biz=MzU2NjU0NjM1Mg==&mid=100000032&idx=1&sn=53e52168caf1ad9e507ab56381c45f1f&chksm=7cab9bff4bdc12e925e282effc1d4993a8652c248abc6169bd31d6fac133628fad54cf516043&mpshare=1&scene=1&srcid=0321CjdEk21qy8WjDgZ0I6sW&key=08039a5457341b11b054342370cc5462829ae3b54e4b265c42e28361773a6fa0e3105d706160d75b097b3ae41148dda265e2416b88f6b6a2391c1f33ec9f0bc62ea9edc86b75344494b598842ad620ac&ascene=1&uin=NzUwMTUxNzIx&devicetype=Windows+10&version=62060739&lang=zh_CN&pass_ticket=%2FD6keuc%2Fx%2Ba8YhupUUvefch8Gm07zVHa34Df5m1waxWQuCOohBN70NNcDEJsKE%2BV"));
-        vwShare.setOnClickListener(view -> {
-            String url = "https://www.coolapk.com/apk/com.gedoor.monkeybook";
-            Single.create((SingleOnSubscribe<Bitmap>) emitter -> {
-                QRCodeEncoder.HINTS.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-                Bitmap bitmap = QRCodeEncoder.syncEncodeQRCode(url, 600);
-                emitter.onSuccess(bitmap);
-            }).compose(RxUtils::toSimpleSingle)
-                    .subscribe(new MySingleObserver<Bitmap>() {
-                        @Override
-                        public void onSuccess(Bitmap bitmap) {
-                            if (bitmap != null) {
-                                moDialogHUD.showImageText(bitmap, url);
-                            }
-                        }
-                    });
-        });
     }
 
     private void joinGroup(String name) {
@@ -247,6 +207,8 @@ public class AboutActivity extends MBaseActivity {
         switch (id) {
             case android.R.id.home:
                 finish();
+                break;
+            default:
                 break;
         }
         return super.onOptionsItemSelected(item);
